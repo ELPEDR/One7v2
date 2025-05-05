6d8be4970624c04b880222243746d6765cc9ec5d
ELPEDR <101681387+ELPEDR@users.noreply.github.com>
2025-05-05 11:55:25 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1746456824,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/openanim-sheet0.png",
+		"images/displayres-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/phone-sheet0.png",
+		"images/settings-sheet0.png",
+		"images/phoneskin-sheet0.png",
+		"images/fadeapps-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
