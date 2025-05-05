7d88ec174e1ce3816ce64d9a1b0b8a2c77509b4c
ELPEDR <101681387+ELPEDR@users.noreply.github.com>
2025-05-05 18:50:51 -0300

    LockScreen

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1746467614,
+	"version": 1746481794,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -26,6 +26,16 @@
 		"images/sprite9-sheet0.png",
 		"images/tiledbackground2.png",
 		"images/sprite10-sheet0.png",
+		"images/sprite11-sheet0.png",
+		"images/sprite13-sheet0.png",
+		"images/sprite14-sheet0.png",
+		"images/tiledbackground3.png",
+		"images/sprite15-sheet0.png",
+		"images/sprite16-sheet0.png",
+		"images/sprite17-sheet0.png",
+		"images/sprite18-sheet0.png",
+		"images/sprite19-sheet0.png",
+		"images/sprite20-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
